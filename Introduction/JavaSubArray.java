129c7312e385b7c4848d60491816bdedaa67aa0d
ishikaA-18 <ishikaagarwalanju@gmail.com>
2026-02-23 17:44:20 +0530

    Create JavaSubArray.java

---
+++ Introduction/JavaSubArray.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Take input
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        // Generate all subarrays
+        for (int i = 0; i < n; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < n; j++) {
+
+                sum = sum + arr[j];
+
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
